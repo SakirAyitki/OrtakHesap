d5fa6ef282dbc4799700ed70c43f6ea8d3dced75
Şakir Ayıtkı <ayitkisakir1@gmail.com>
2025-03-21 16:59:03 +0300

    App created

---
+++ LoginScreen.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Login Screen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
