47a7a47ad1e4b75b3174ff48d713339f63171344
Şakir Ayıtkı <ayitkisakir1@gmail.com>
2025-03-21 19:05:35 +0300

    Project template designed

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import LoginScreen from "./LoginScreen";
+import LoginScreen from "./src/screens/auth/LoginScreen";
 
 export default function App() {
   return (
---
+++ src/utils/color.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+    PRIMARY: '#71C9CE',      // En koyu turkuaz (ana işlemler için)
+    SECONDARY: '#A6E3E9',    // Orta-koyu turkuaz (ikincil işlemler için)
+    TERTIARY: '#CBF1F5',     // Açık turkuaz (vurgu alanları için)
+    BACKGROUND: '#E3FDFD',   // En açık turkuaz (uygulama arkaplanı)
+    
+    // Tamamlayıcı renkler
+    TEXT_DARK: '#2c3e50',    // Koyu metin
+    TEXT_LIGHT: '#FFFFFF',   // Açık metin
+    POSITIVE: '#4CAF50',     // Pozitif değerler (alacaklar)
+    NEGATIVE: '#FF5252',     // Negatif değerler (borçlar)
+    BORDER: '#A6E3E9',       // Sınırlar/ayraçlar
+    SHADOW: 'rgba(113,201,206,0.2)' // Gölgeler için
+  };
