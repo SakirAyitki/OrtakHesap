b8b6c3f97639cee3ba89f88e110ff5df13c52c55
Şakir Ayıtkı <ayitkisakir1@gmail.com>
2025-04-10 21:32:25 +0300

    Remove AuthProvider wrapper from App component

--- App.tsx
+++ App.tsx
@@ -1,19 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "react-native";
 import AuthNavigator from "./src/navigation/AuthNavigator";
-import { AuthProvider } from "./src/context/AuthContext";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <StatusBar
-          barStyle="dark-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        <AuthNavigator />
-      </NavigationContainer>
-    </AuthProvider>
+    <NavigationContainer>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <AuthNavigator />
+    </NavigationContainer>
   );
 }
